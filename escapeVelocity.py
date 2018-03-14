15123ae0bdd4667b4b1574ebb1906466ccc24d07
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-14 10:55:06 -0400

    Updated from Brython Server: 3/14/2018 10:55:05 AM

--- escapeVelocity.py
+++ escapeVelocity.py
@@ -4,14 +4,18 @@
 
 from ggrocket import Rocket, Planet
 from math import radians, sqrt
+from ggame import Slider
+
+tz = Slider((10,400), 0, 5, 0, positioning="physical")
 
 Re = 6.371E6
 Me = 5.972E24
 G = 6.674E-11
 
+earth = Planet(viewscale=0.01)
 Ve=sqrt(2*Me*G/Re)
 print("Predicted escape velocity is ", Ve, "m/s")
+rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=Ve, timezoom=1, timezoom=tz)
 
-earth = Planet(viewscale=0.01)
-rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=Ve, timezoom=2)
+
 earth.run(rocket)
