1d5c69f2d635e7cf941ac7e8af93c951e190b195
samkrimmel <35492772+samkrimmel@users.noreply.github.com>
2018-03-15 10:34:51 -0400

    Updated from Brython Server: 3/15/2018 10:34:50 AM

--- rocket4.py
+++ rocket4.py
@@ -6,7 +6,6 @@ from ggrocket import Rocket, Planet
 from math import radians, sqrt, log
 from ggmath import InputButton, Timer, Label, Slider
 
-earth = Planet(planetmass=0)
 
 Stage1Started = False
 Stage2Started = False
